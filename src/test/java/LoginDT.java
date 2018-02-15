ab167a9e0eb1dafe3e075cb2961d2981b9d323d2
saberc <sabrina.ecca@gmail.com>
2018-02-15 17:36:54 +0100

    Test Login Button for DT App

---
+++ src/test/java/LoginDT.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class LoginDT {
+
+  @Test
+  public void loginaction() {
+      String username="admin";
+      String password = "adminadmin";
+     WebDriver driver  = new ChromeDriver();
+      driver.get("http://designtime.serv.run/");
+      
+      WebElement UserName= driver.findElement(By.id("username"));
+      WebElement PassWord= driver.findElement(By.id("password"));
+      
+      Actions builder = new Actions(driver);
+      
+      Actions fillUsername = builder.moveToElement(UserName).click().sendKeys(username);
+      fillUsername.perform();
+      Actions fillPassword = builder.moveToElement(PassWord).click().sendKeys(password);
+      fillPassword.perform();
+      
+      WebElement acceptAnswerLink = driver.findElement(By.className("LoginForm__loginButton"));
+      acceptAnswerLink.click();
+      
+      
+    
+    //driver.quit();
+  }
+}
