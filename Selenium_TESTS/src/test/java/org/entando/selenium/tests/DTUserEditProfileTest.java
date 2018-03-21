21e7a3e1b312e2702020bb709a15eb7bfd3aaf78
Leobel Izquierdo <leobelizquierdo@gmail.com>
2018-03-21 19:24:20 -0400

    add edit user profile test

---
+++ Selenium_TESTS/src/test/java/org/entando/selenium/tests/DTUserEditProfileTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.entando.selenium.tests;
+
+import org.entando.selenium.pages.DTDashboardPage;
+import org.entando.selenium.pages.DTLoginPage;
+import org.entando.selenium.utils.FunctionalTest;
+import org.entando.selenium.utils.ReceiptDTLoginPage;
+import org.entando.selenium.utils.Utils;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+/**
+ *
+ * @author leobel
+ */
+public class DTUserEditProfileTest extends FunctionalTest {
+    
+    @Test
+    public void runtTest(){
+        DTLoginPage dtLoginPage = new DTLoginPage(driver);
+        dtLoginPage.logIn("admin", "adminadmin");
+
+        ReceiptDTLoginPage receiptDtPage = dtLoginPage.submit();
+        assertTrue(receiptDtPage.isInitialized());
+        
+        DTDashboardPage dtDashboardPage = new DTDashboardPage(driver);
+        dtDashboardPage.SelectSecondOrderLink("User Settings", "Users");
+        
+        Utils util = new Utils();
+        WebElement table = driver.findElement(By.className("UserListTable__table"));
+        String user = "admin";
+        Utils.Kebab kebab = util.getKebabOnTable(table, "Username", user, "button");
+        kebab.getClickable().click();
+        
+        util.waitUntilVisible(driver, kebab.getActionList());
+        
+        util.clickKebabActionOnList(kebab.getActionList(), "Edit profile of: " + user);
+    }
+}
