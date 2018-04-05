3e61e8687b1f63eb10745c55eab5b7afc9cc47e7
Pietrangelo Masala <pietrangelo@users.noreply.github.com>
2018-04-05 11:52:06 +0200

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## env variables
+envDir="postman_API/environment"
+envFile="Entando5API.postman_environment.json"
+
+for collection in postman_API/*.json; do
+    newman run $collection -e $envDir/$envFile --reporters cli,junit --reporter-junit-export newman.xml
+done
