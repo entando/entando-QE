6a3b003defd3af920bfea0d1ca2a9f0146f853d0
Pietrangelo Masala <pietrangelo@users.noreply.github.com>
2018-04-05 12:32:19 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ envDir="postman_API/environment"
 envFile="Entando5API.postman_environment.json"
 
 for collection in postman_API/*.json; do
-    newman run $collection -e $envDir/$envFile --reporters cli,junit --reporter-junit-export newman.xml
+    newman run $collection -e $envDir/$envFile --reporters cli,junit --reporter-junit-export newman.xml --suppress-exit-code
 done
